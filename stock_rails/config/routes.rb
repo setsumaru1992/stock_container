7ac4ce3c2092efede565cf5f5fb8ae61a4a3cfc8
setsumaru <setsumaru@moyamoya.space>
2019-08-27 09:35:23 +0900

    サブディレクトリパスを追加

--- stock_rails/config/routes.rb
+++ stock_rails/config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  namespace :view do
-    get 'stock_category/list'
-  end
-  root to: "view/stock#search"
-  get      "debug/debug"
 
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-  }
+  scope :stockapp do
+    namespace :view do
+      get 'stock_category/list'
+    end
+    root to: "view/stock#search"
+    get "debug/debug"
 
-  namespace :bot do
-    get    "regist_new_stocks"
-    get    "regist_or_update_stocks"
-    get    "regist_or_update_stock"
-    get    "regist_stock_prices"
-    get    "notice_bought_stock_prices"
-    get    "notice_bought_and_favorite_stocks_with_chart"
-  end
+    devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'
+    }
 
-  namespace :view do
-    get    "stock/search"
+    namespace :bot do
+      get "regist_new_stocks"
+      get "regist_or_update_stocks"
+      get "regist_or_update_stock"
+      get "regist_stock_prices"
+      get "notice_bought_stock_prices"
+      get "notice_bought_and_favorite_stocks_with_chart"
+    end
+    namespace :view do
+      get "stock/search"
+    end
   end
 end
